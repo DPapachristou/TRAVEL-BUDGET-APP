fda0bd080d93361ae6d728671d07e338a4dddb6a
DPapachristou <d.k.papachristou@gmail.com>
2023-07-04 01:14:17 +0300

    NavBar Update

--- travel-budget-app/src/COMPONENTS/Navbar.js
+++ travel-budget-app/src/COMPONENTS/Navbar.js
@@ -14,14 +14,14 @@ function Header() {
       data-bs-theme="light"
     >
       <Container>
-        <Navbar.Brand href="/">TRAVEL BUDGET APP</Navbar.Brand>
+        <Navbar.Brand href="/">TRAVEL BUDGET</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">HOME</Nav.Link>
             <Nav.Link href="/balance">BALANCE</Nav.Link>
             <Nav.Link href="/history">HISTORY</Nav.Link>
-            <NavDropdown title="MY ACCOUNT" id="basic-nav-dropdown">
+            <NavDropdown title="ACCOUNT" id="basic-nav-dropdown">
               <NavDropdown.Item href="/login">LOGIN</NavDropdown.Item>
               <NavDropdown.Item href="/myAccount">MY ACCOUNT</NavDropdown.Item>
               <NavDropdown.Divider />
